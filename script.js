299c6de4c338f2b7baa4065c831fee64c564e246
Calebcmf <caleb_freitas@estudante.sesisenai.org.br>
2023-05-04 11:42:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function Confirmar()
+{
+    texto = nome.value //valor do input
+    teste.innerHTML = texto //valor da div = valor da variável
+}
+
+function MostrarJanela()
+{
+    janela.style.display = "block"
+}
+
+function EsconderJanela()
+{
+    janela.style.display = "none"
+}
+soma = 0
+function Somar()
+{
+    soma = soma + 1
+    resultado.value = soma
+}
+
+function Imprimir()
+{
+    window.print();
+}
